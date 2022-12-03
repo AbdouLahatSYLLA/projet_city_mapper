c1e70d0c42475bc47aadeb2258d166bdf127af94
Abdou Sylla <abdoulahat.sylla@edu.univ-paris13.fr>
2022-12-03 02:52:05 +0100

    lire le readme et commencer

--- parser/parse_sections.py
+++ parser/parse_sections.py
@@ -8,7 +8,5 @@ for line in gj['features']:
       a = str(elt)
       b = str(line['properties']['route_I_counts'][elt])
       stri +=a+":"+b+','
-   stri = stri.rstrip(',')   
+   stri = stri.rstrip(',')
    print(f"INSERT INTO sections VALUES('{line['geometry']['coordinates']}','{line['properties']['from_stop_I']}','{line['properties']['to_stop_I']}','{line['properties']['n_vehicles']}','{line['properties']['duration_avg']}','{stri}','{line['properties']['route_type']}');")
-
-
