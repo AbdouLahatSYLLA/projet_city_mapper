3ae756f07db47da6551692022c91f73534970584
Abdou Sylla <abdoulahat.sylla@edu.univ-paris13.fr>
2022-12-04 16:17:29 +0100

    lire le readme, et comprendre le mapsmetro.py au niveau de de hops>=1

--- parser/parse_routes.py
+++ parser/parse_routes.py
@@ -1,6 +1,6 @@
 import geojson
-geojson.geometry.Geometry.__init__.__defaults__ = (None, False, 20)
-with open("routes.geojson") as f:
+#geojson.geometry.Geometry.__init__.__defaults__ = (None, False, 20)
+with open("schema_sql/routes.geojson") as f:
    gj = geojson.load(f)
 for line in gj['features']:
    print(f"INSERT INTO routes VALUES('{line['geometry']['coordinates']}','{line['properties']['route_type']}','{line['properties']['route_I']}','{line['properties']['route_name']}');")
--- schema_sql/db-city_mapper_schema.sql
+++ schema_sql/db-city_mapper_schema.sql
@@ -1,5 +1,5 @@
 
-/*CREATE DATABASE paris_mapper;*/
+/*CREATE DATABASE city_mapper;*/
 
 
 CREATE TABLE network_bus ( from_stop_I NUMERIC(10,2),to_stop_I NUMERIC(10,2),d NUMERIC(10,2),duration_avg NUMERIC(10,2),n_vehicles NUMERIC(10,2), route_I_counts TEXT, PRIMARY KEY (from_stop_I,to_stop_I) );
@@ -10,10 +10,10 @@ CREATE TABLE network_subway ( from_stop_I NUMERIC(10,2),to_stop_I NUMERIC(10,2),
 
 CREATE TABLE network_tram ( from_stop_I NUMERIC(10,2),to_stop_I NUMERIC(10,2),d NUMERIC(10,2),duration_avg NUMERIC(10,2),n_vehicles NUMERIC(10,2), route_I_counts TEXT,PRIMARY KEY (from_stop_I,to_stop_I) );
 
-CREATE TABLE stops ( stop_I NUMERIC(10,2),latitude NUMERIC(10,2) ,longitude NUMERIC(10,2) ,nom VARCHAR(100), PRIMARY KEY (stop_I) );
+CREATE TABLE network_node ( stop_I NUMERIC(10,2),latitude NUMERIC (10, 6) ,longitude NUMERIC (10, 6) ,nom VARCHAR(100), PRIMARY KEY (stop_I) );
 
-CREATE TABLE network_walk (from_stop_I NUMERIC(10,2),to_stop_I NUMERIC(10,2),d NUMERIC(10,2),d_walk NUMERIC(10,2) );
+CREATE TABLE network_walk (from_stop_I NUMERIC (10, 6),to_stop_I NUMERIC (10, 6),d NUMERIC (10, 6),d_walk NUMERIC (10, 6) );
 
-CREATE TABLE ROUTES (coordinates text , route_type NUMERIC(2,0),route_I NUMERIC(10,2), route_name NUMERIC(10,2));
+CREATE TABLE ROUTES (coordinates text , route_type NUMERIC(2,0),route_I NUMERIC (10, 6), route_name VARCHAR(32));
 
-CREATE TABLE moyen_transport(mode VARCHAR(32), id NUMERIC(5,0), PRIMARY KEY(id) );
+CREATE TABLE moyen_transport(mode VARCHAR(32), id NUMERIC(8,0), PRIMARY KEY(id) );
