56d356745876ba6e58a975cbeb32981889b05ae0
Daniel Tharmarajah <daniel.tharmarajah@edu.univ-paris13.fr>
2022-12-17 19:18:24 +0100

    MAJ parse sections

--- parser/parse_sections.py
+++ parser/parse_sections.py
@@ -3,10 +3,11 @@ geojson.geometry.Geometry.__init__.__defaults__ = (None, False, 20)
 with open("sections.geojson") as f:
    gj = geojson.load(f)
 for line in gj['features']:
-   stri = ""
+   routes = []
    for elt in line['properties']['route_I_counts']:
-      a = str(elt)
-      b = str(line['properties']['route_I_counts'][elt])
-      stri +=a+":"+b+','
-   stri = stri.rstrip(',')
-   print(f"INSERT INTO sections VALUES('{line['geometry']['coordinates']}','{line['properties']['from_stop_I']}','{line['properties']['to_stop_I']}','{line['properties']['n_vehicles']}','{line['properties']['duration_avg']}','{stri}','{line['properties']['route_type']}');")
+      a = [int(str(elt))]
+      routes += a
+      
+   print(f"INSERT INTO sections VALUES('{line['geometry']['coordinates']}','{line['properties']['from_stop_I']}','{line['properties']['to_stop_I']}','{line['properties']['n_vehicles']}','{line['properties']['duration_avg']}','{routes}','{line['properties']['route_type']}');")
+
+
